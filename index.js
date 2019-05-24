38c472d1b8dd12cb1f73741b957ec76363ef9c7f
jack <hao3304@qq.com>
2019-05-24 08:21:12 +0800

    test

--- index.js
+++ index.js
@@ -1,40 +1,65 @@
 const elasticsearch = require("elasticsearch");
 
+const hosts = ["202.121.179.42"];
+
 const esClient = new elasticsearch.Client({
-  hosts: ["202.121.179.42:9200"],
-  log: "trace"
+  hosts: hosts.map(host => host + ":9200"),
+  log: "error"
 });
 
-esClient.scroll(
-  {
-    scroll: "200m"
-  },
-  function(e) {
-    debugger;
-  },
-  function(err) {
-    debugger;
-  }
-);
+// esClient.clearScroll();
 
-// esClient
-//   .search({
-//     index: "mail-*",
-//     body: {
-//       query: {
-//         match: {
-//           loghost: "202.120.0.7"
-//         }
-//       }
-//     },
-//     scroll: "200m",
-//     size: 100
-//   })
-//   .then(
-//     function(re) {
-//       debugger;
-//     },
-//     function(err) {
-//       console.trace(err.message);
-//     }
-//   );
+function init() {
+  esClient
+    .search({
+      index: "mail-*",
+      body: {
+        query: {
+          bool: {
+            should: [
+              {
+                match: {
+                  loghost: "202.120.0.7"
+                }
+              },
+              {
+                match: {
+                  loghost: "202.120.0.8"
+                }
+              }
+            ]
+          }
+        },
+        sort: {
+          "@timestamp": {
+            order: "desc"
+          }
+        }
+      },
+      scroll: "200m",
+      size: 100
+    })
+    .then(
+      function(re) {
+        debugger;
+        doScroll(re._scroll_id);
+      },
+      function(err) {
+        console.trace(err.message);
+      }
+    );
+}
+
+function doScroll(scroll_id) {
+  esClient.scroll(
+    {
+      scroll_id: scroll_id
+    },
+    function(e) {
+      debugger;
+    },
+    function(err) {}
+  );
+}
+
+init();
